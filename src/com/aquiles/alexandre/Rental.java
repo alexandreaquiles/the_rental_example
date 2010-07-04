66cf566ebc169a3172d93a33f622b1e76fb74e36
Alexandre Aquiles <alexandre.aquiles@gmail.com>
2010-07-04 12:09:43 -0300

    Changing getFrequentRenterPoints implementation, aiming to remove frequentRenterPoints variable. Tests failed.

--- src/com/aquiles/alexandre/Rental.java
+++ src/com/aquiles/alexandre/Rental.java
@@ -41,13 +41,10 @@ public class Rental {
 	}
 	
 	int getFrequentRenterPoints(int frequentRenterPoints) {
-		//add frequent renter points
-		frequentRenterPoints++;
-
-		//add bonus for a two day new release rental
 		if(getMovie().getPriceCode() == Movie.NEW_RELEASE && getDaysRented() > 1)
-			frequentRenterPoints++;
-		return frequentRenterPoints;
+			return 2;
+		else 
+			return 1;
 	}
 
 	
