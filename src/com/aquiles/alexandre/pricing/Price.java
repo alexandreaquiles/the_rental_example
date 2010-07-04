d3afd5c183c18c411cbcb4a8e3236c7c49249e87
Alexandre Aquiles <alexandre.aquiles@gmail.com>
2010-07-04 18:22:06 -0300

    Removing redundant implementations in Price's getFrequentRenterPoints. And we're done.

--- src/com/aquiles/alexandre/pricing/Price.java
+++ src/com/aquiles/alexandre/pricing/Price.java
@@ -1,8 +1,5 @@
 package com.aquiles.alexandre.pricing;
 
-import com.aquiles.alexandre.Movie;
-
-
 public abstract class Price {
 
 	public abstract Integer getPriceCode();
@@ -10,9 +7,6 @@ public abstract class Price {
 	public abstract Double getCharge(Integer daysRented);
 
 	public Integer getFrequentRenterPoints(Integer daysRented) {
-		if(getPriceCode() == Movie.NEW_RELEASE && daysRented > 1)
-			return 2;
-		else 
-			return 1;
+		return 1;
 	}
 }
